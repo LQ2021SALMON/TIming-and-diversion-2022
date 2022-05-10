484bd21cc3e6259e5819f6041fc0d97e1da84091
Qi Liu <qi.liu@dfo-mpo.gc.ca>
2022-05-10 14:54:44 -0700

    update run files

--- timing_diversion_forecasting/RUN/oscar_read_example.R
+++ timing_diversion_forecasting/RUN/oscar_read_example.R
@@ -2,7 +2,7 @@
 
 #a simple example showing the import of oscar current vectors u and v.
 
-setwd("C:/Users/liuq/Desktop/salmon migration timing/timing_diversion_forecasting/timing_diversion_forecasting/DATAIN")
+setwd("C:/Users/liuq/Desktop/Timing-Diversion-2022/Timing-and-Diversion-2022/timing_diversion_forecasting/DATAIN")
 rm(list = ls(all=T))
 library(tidyverse)
 # data:grid means ---------------------------------------------------------
@@ -15,10 +15,63 @@ data.gridmean.wide <- merge(data.gridmean[[1]]$data.wide, data.gridmean[[2]]$dat
 
 str(data.gridmean.wide)
 
-data.gridmean.wide.area1 <- filter(data.gridmean.wide, group == 1, month== 1)
+gridmean.wide.area1_Jan <- filter(data.gridmean.wide, group == 1, month== 1, year >=1993 & year <= 2021)
+#write.csv(gridmean.wide.area1_Jan, file = "gridmean.wide.A1Jan.csv")
+gridmean.wide.area2_Jan <- filter(data.gridmean.wide, group == 2, month== 1, year >=1993 & year <= 2021)
+#write.csv(gridmean.wide.area2_Jan, file = "gridmean.wide.A2Jan.csv")
+gridmean.wide.area3_Jan <- filter(data.gridmean.wide, group == 3, month== 1, year >=1993 & year <= 2021)
+#write.csv(gridmean.wide.area3_Jan, file = "gridmean.wide.A3Jan.csv")
+# gridmean.wide.area1_Apr <- filter(data.gridmean.wide, group == 1, month== 4)
+# gridmean.wide.area1_May <- filter(data.gridmean.wide, group == 1, month== 5)
+# gridmean.wide.area1_Jun <- filter(data.gridmean.wide, group == 1, month== 6)
+# gridmean.wide.area1_Jul <- filter(data.gridmean.wide, group == 1, month== 7)
+# gridmean.wide.area1_Aug <- filter(data.gridmean.wide, group == 1, month== 8)
+# gridmean.wide.area1_Sep <- filter(data.gridmean.wide, group == 1, month== 9)
+# gridmean.wide.area1_Oct <- filter(data.gridmean.wide, group == 1, month== 10)
+# gridmean.wide.area1_Nov <- filter(data.gridmean.wide, group == 1, month== 11)
+# gridmean.wide.area1_Dec <- filter(data.gridmean.wide, group == 1, month== 12)
+
+# timing/day for earlystuart and chilko 1993-2021
+sockeye.timing <- read.csv("ChilkoTimingForecastData.csv")
 
 
+early.stuart.df <- filter(sockeye.timing, Stock == "earlystuart", year >= 1993)
+early.stuart.timing <- select(early.stuart.df, A20.day)
+                       
+                       
+chilko.df <- filter(sockeye.timing, Stock == "chilko", year >= 1993)
+chilko.timing <- select(chilko.df, A20.day)
 
+#linear regression between timing and current velocity in January of return year using 3 areas
+# for early stuart
+df.test <- read.csv("timing_velocity_test.csv")
+
+ES.A1Jan.u.reg <- lm(estuart.day ~ gridmean.A1Jan.u, df.test)
+ES.A1Jan.v.reg <- lm(estuart.day ~ gridmean.A1Jan.v, df.test)
+summary(ES.A1Jan.u.reg)
+summary(ES.A1Jan.v.reg)
+ES.A2Jan.u.reg <- lm(estuart.day ~ gridmean.A2Jan.u, df.test)
+ES.A2Jan.v.reg <- lm(estuart.day ~ gridmean.A2Jan.v, df.test)
+summary(ES.A2Jan.u.reg)
+summary(ES.A2Jan.v.reg)
+ES.A3Jan.u.reg <- lm(estuart.day ~ gridmean.A3Jan.u, df.test)
+ES.A3Jan.v.reg <- lm(estuart.day ~ gridmean.A3Jan.v, df.test)
+summary(ES.A3Jan.u.reg)
+summary(ES.A3Jan.v.reg)
+
+#for chilko
+CK.A1Jan.u.reg <- lm(chilko.day ~ gridmean.A1Jan.u, df.test)
+CK.A1Jan.v.reg <- lm(chilko.day ~ gridmean.A1Jan.v, df.test)
+summary(CK.A1Jan.u.reg)
+summary(CK.A1Jan.v.reg)
+CK.A2Jan.u.reg <- lm(chilko.day ~ gridmean.A2Jan.u, df.test)
+CK.A2Jan.v.reg <- lm(chilko.day ~ gridmean.A2Jan.v, df.test)
+summary(CK.A2Jan.u.reg)
+summary(CK.A2Jan.v.reg)
+CK.A3Jan.u.reg <- lm(chilko.day ~ gridmean.A3Jan.u, df.test)
+CK.A3Jan.v.reg <- lm(chilko.day ~ gridmean.A3Jan.v, df.test)
+summary(CK.A3Jan.u.reg)
+summary(CK.A3Jan.v.reg)
 
 # data:monthly means ------------------------------------------------------
 
